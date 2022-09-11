35fd0dfcff4e7666fea74860e4fb9e60e51bba38
g200045 <liang.huang@gareatech.com>
2022-09-11 16:06:16 +0800

    add hsp

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,6 +3,29 @@
     <head-bar></head-bar>
     <div class="banner">
       <img src="../assets/image/banner.png" alt="" />
+      <div class="video">
+        <video controls height="694" src="../assets/about.mp4"></video>
+      </div>
+    </div>
+    <div class="hsp-banner">
+      <div class="common-title">
+        <p class="first">MEDICAL CENTER</p>
+        <p class="sec">医学中心</p>
+      </div>
+      <ul class="card-list">
+        <li>
+          <img src="../assets/image/card/1.png" alt="" />
+        </li>
+        <li>
+          <img src="../assets/image/card/2.png" alt="" />
+        </li>
+        <li>
+          <img src="../assets/image/card/3.png" alt="" />
+        </li>
+        <li>
+          <img src="../assets/image/card/4.png" alt="" />
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -34,8 +57,59 @@
 .banner {
   margin: 0 auto;
   max-width: 1920px;
+  position: relative;
   img {
     width: 100%;
+  }
+  .video {
+    height: 700px;
+    position: absolute;
+    left: 50%;
+    top: 0;
+    height: 700px;
+    text-align: center;
+    box-sizing: border-box;
+    transform: translate(-50%, 0);
+  }
+}
+.hsp-banner {
+  text-align: center;
+  background: url("../assets/image/hsp_banner.png") no-repeat;
+  background-size: 100%;
+  max-width: 1920px;
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      flex: 1;
+      width: 255px;
+      height: 176px;
+      margin-right: 30px;
+      &:nth-child(4n) {
+        margin-right: 0;
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+.common-title {
+  text-align: center;
+  margin-top: 50px;
+  .first {
+    font-size: 60px;
+    font-weight: bold;
+    color: #333333;
+    opacity: 0.3;
+  }
+  .sec {
+    font-size: 48px;
+    color: #333333;
+    position: relative;
+    z-index: 1;
+    top: -60px;
+    font-weight: bold;
   }
 }
 @media (max-width: 768px) {
